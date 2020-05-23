9986318e21eea8adb95ec7cb1dd1e881af2dda39
unknown <ethanls@protonmail.com>
2020-05-23 00:56:05 -0700

    fist commit test

---
+++ src/routes/_layout.svelte
@@ -0,0 +1,23 @@
+<script>
+  import Nav from "../components/Nav.svelte";
+
+  export let segment;
+</script>
+
+<style>
+  main {
+    position: relative;
+    /* max-width: 56em; */
+    background-color: white;
+    padding: 2em;
+    box-sizing: border-box;
+    background-color: red;
+    width: 100vw;
+    height: 100vh;
+  }
+</style>
+
+<Nav {segment} />
+<main>
+  <slot />
+</main>
---
+++ src/routes/index.svelte
@@ -0,0 +1,65 @@
+<script>
+  const defendants = [
+    { First: "John", Last: "Manager", Posted_date: "9/10/15" },
+    { First: "Joe", Last: "Manager", Posted_date: "9/10/15" },
+    { First: "Jane", Last: "Manager", Posted_date: "9/10/15" },
+    { First: "Ethan", Last: "Manager", Posted_date: "9/10/15" },
+    { First: "Joshua", Last: "Manager", Posted_date: "9/10/15" },
+    { First: "Jimmy", Last: "Manager", Posted_date: "9/10/15" },
+    { First: "Max", Last: "Manager", Posted_date: "9/10/15" },
+    { First: "Rich", Last: "Manager", Posted_date: "9/10/15" },
+    { First: "Jake", Last: "Manager", Posted_date: "9/10/15" },
+    { First: "Ezra", Last: "Manager", Posted_date: "9/10/15" }
+  ];
+</script>
+
+<style>
+  .container {
+    display: flex;
+    flex-direction: column;
+  }
+  .defendant {
+    background-color: #212121;
+    color: white;
+    padding: 10px;
+    flex: 1;
+    min-width: 90%;
+    max-width: 100%;
+    margin: auto;
+    margin-bottom: 10px;
+  }
+
+  button {
+    width: 75px;
+    height: auto;
+    background-color: #333;
+    border: none;
+    border-radius: 5px;
+  }
+
+  button:hover {
+    background-color: rgb(13, 146, 255);
+  }
+
+  button:hover > .material-icons {
+    color: white;
+  }
+</style>
+
+<svelte:head>
+  <title>Defendants</title>
+</svelte:head>
+
+<div class="container">
+  {#each defendants as defendant}
+    <div class="defendant">
+      <div class="card" style="display: flex">
+        <h1 style="flex:3">{defendant.First} {defendant.Last}</h1>
+        <p style="flex:1">{defendant.Posted_date}</p>
+        <button>
+          <span class="material-icons">remove_red_eye</span>
+        </button>
+      </div>
+    </div>
+  {/each}
+</div>
